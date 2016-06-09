83e774a456543b5eeb7dfe72f1f26abb1a8503eb
stahl <office@stahlnow.com>
2016-06-09 02:35:09 +0200

    fix gallery migration

--- website/apps/gallery/migrations/0001_initial.py
+++ website/apps/gallery/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import filer.fields.image
 import django.utils.timezone
 import django_extensions.db.fields
 
@@ -9,10 +10,27 @@ import django_extensions.db.fields
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('fileupload', '0005_auto_20150330_1902'),
+        ('filer', '0002_auto_20150606_2003'),
     ]
 
     operations = [
+        migrations.CreateModel(
+            name='Gallery',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('uuid', django_extensions.db.fields.UUIDField(editable=False, name=b'uuid', blank=True)),
+                ('created', django_extensions.db.fields.CreationDateTimeField(default=django.utils.timezone.now, editable=False, blank=True)),
+                ('updated', django_extensions.db.fields.ModificationDateTimeField(default=django.utils.timezone.now, editable=False, blank=True)),
+                ('name', models.CharField(max_length=1024)),
+            ],
+            options={
+                'ordering': ('-created',),
+                'db_table': 'gallery_galleries',
+                'verbose_name': 'gallery',
+                'verbose_name_plural': 'galleries',
+            },
+            bases=(models.Model,),
+        ),
         migrations.CreateModel(
             name='Image',
             fields=[
@@ -20,7 +38,10 @@ class Migration(migrations.Migration):
                 ('uuid', django_extensions.db.fields.UUIDField(editable=False, name=b'uuid', blank=True)),
                 ('created', django_extensions.db.fields.CreationDateTimeField(default=django.utils.timezone.now, editable=False, blank=True)),
                 ('updated', django_extensions.db.fields.ModificationDateTimeField(default=django.utils.timezone.now, editable=False, blank=True)),
-                ('image', models.ForeignKey(blank=True, to='fileupload.File', null=True)),
+                ('caption', models.TextField(null=True, verbose_name='caption', blank=True)),
+                ('link', models.URLField(null=True, verbose_name='link', blank=True)),
+                ('gallery', models.ForeignKey(related_name='images', blank=True, to='gallery.Gallery', null=True)),
+                ('image', filer.fields.image.FilerImageField(blank=True, to='filer.Image', null=True)),
             ],
             options={
                 'ordering': ('-created',),
